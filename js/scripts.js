166009d71a63501853881f84298171948aaaf6d7
Drex13 <79420374+Drex13@users.noreply.github.com>
2021-09-06 22:15:01 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+    document.getElementById("move-content").classList.toggle('move-container-all');
+}
